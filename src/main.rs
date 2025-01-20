a3c14639176a58afd93458970841f7889cb357fb
Aarni Koskela <akx@iki.fi>
2025-01-20 13:31:14 +0200

    wip

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+// When compiling natively:
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "varjostin",
+        native_options,
+        Box::new(|cc| Ok(Box::new(varjostin::TemplateApp::new(cc)))),
+    )
+}
